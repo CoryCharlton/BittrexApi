8b5a1e83123171859baf1e5ae57f70e6749d1c95
Cory Charlton <ccharlton@pobox.com>
2017-09-02 20:40:04 -0700

    Trying to get code coverage working

--- library/src/androidTest/java/com/corycharlton/bittrexapi/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/com/corycharlton/bittrexapi/ExampleInstrumentedTest.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -21,6 +23,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.corycharlton.bittrexapi", appContext.getPackageName());
+        assertNotNull(appContext);
     }
 }
