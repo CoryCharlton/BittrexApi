619628ca60d1c5f2e6580e682565df0c866a6404
Cory Charlton <ccharlton@pobox.com>
2017-09-02 21:04:48 -0700

    Changing the Travis CI cache settings

--- library/src/androidTest/java/com/corycharlton/bittrexapi/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/com/corycharlton/bittrexapi/ExampleInstrumentedTest.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
